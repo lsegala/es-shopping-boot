f629c80c10198bae10714192bc5f3ec161b66358
Çağatay Çivici <cagatay.civici@github.com>
2016-05-10 00:40:43 +0300

    Update quickstart to Angular2 RC

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,5 @@
-import {Component} from 'angular2/core';
-import {HTTP_PROVIDERS} from 'angular2/http';
+import {Component} from '@angular/core';
+import {HTTP_PROVIDERS} from '@angular/http';
 import {InputText,DataTable,Button,Dialog,Column,Header,Footer} from 'primeng/primeng';
 import {Car} from './cars/car';
 import {CarService} from './cars/carservice';
